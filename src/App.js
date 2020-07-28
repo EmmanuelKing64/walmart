c7688c2f45a4278f488ee280049c49b24ddd3d86
EL Sundararaj <34271@ITCINFOTECH.COM>
2020-07-28 19:14:15 +0530

    store file updated

--- src/App.js
+++ src/App.js
@@ -2,19 +2,8 @@ import React, { useReducer } from "react";
 import RecipeReviewCard from "./component/CardView";
 import Defined from "./component/defined";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { AppContext, initialState } from "./store";
-const reducer = (state = initialState, action) => {
-  const { type, payload } = action;
-  switch (type) {
-    case "ACTION":
-      console.log("action is called");
-      return { ...state, keyProducts: payload };
-    case "SET_PRODUCTS":
-      return { ...state, entities: payload};
-    default:
-      console.log("Unknown action");
-  }
-};
+import { AppContext, reducer } from "./store";
+
 function App() {
   console.log('app')
   const [value, dispatch] = useReducer(reducer);
